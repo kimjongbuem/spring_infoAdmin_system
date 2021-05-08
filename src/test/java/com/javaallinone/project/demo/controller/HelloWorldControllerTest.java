46eff64e7e95c509fdc4949e85d04a2ade195b7f
kimjongbuem <992mseven@naver.com>
2021-05-08 15:16:07 +0900

    @Test 하는방법.
    @Test
        public void mockMvcTest() throws Exception {
    
            mockMvc.perform(
                    MockMvcRequestBuilders.get("/hello")
            ).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
                    .andExpect(MockMvcResultMatchers.content().string("helloworld"));
        }

---
+++ src/test/java/com/javaallinone/project/demo/controller/HelloWorldControllerTest.java
@@ -0,0 +1,43 @@
+package com.javaallinone.project.demo.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloWorldControllerTest {
+
+    @Autowired
+    private HelloWorldController helloWorldController;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWorldController).build();
+    }
+
+    @Test
+    public void helloWorld(){
+        assertThat(helloWorldController.helloWorld()).isEqualTo("helloworld");
+    }
+
+    @Test
+    public void mockMvcTest() throws Exception {
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/hello")
+        ).andDo(MockMvcResultHandlers.print()).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("helloworld"));
+    }
+}
